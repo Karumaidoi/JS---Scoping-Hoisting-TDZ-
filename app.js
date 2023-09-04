b5a1908aaa0da2afb96a29346c679bf76108e28e
Karumaidoi <mainahmwangi12@gmail.com>
2023-09-04 22:47:57 +0300

    Done - lecture

--- app.js
+++ app.js
@@ -1,4 +1,36 @@
 "use strict";
 
 //////////////////////////////////////
-// HOISTING AND TEMPORAL DEAD ZONE
+// HOISTING AND TEMPORAL DEAD ZONE, THIS
+// var, let, const,
+// Declaration, Expresson and Arrow
+
+console.log(window.name);
+
+console.log(name);
+// console.log(name2);
+// console.log(name3);
+
+var name = "Alex"; // Global Scope
+const name2 = "Maina"; // Undefined
+let name3 = "Mwangi"; // undefined
+
+// class App {
+//   addDecl() {
+//     console.log(this); // Undefined
+//   }
+
+//   addExp = function () {
+//     console.log(this);
+//   };
+
+//   checkArr() {
+//     addArr = () => console.log(this);
+//   }
+// }
+
+// const app = new App();
+
+// app.addArr();
+// app.addDecl();
+// app.addExp();
